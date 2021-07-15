426f4acca2932a561c790df8d3439ab8425231ba
suemoto-kouhei <86272437+suemoto-kouhei@users.noreply.github.com>
2021-07-15 15:51:12 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,71 @@
+// header > nav
+// ==================================================
+$(function(){
+  $('#nav-btn').on('click',function(){
+      $(this).toggleClass('-active');
+      $('.nav').toggleClass('-active');
+  });
+});
+
+
+// animation / wow.js
+// ==================================================
+window.addEventListener('DOMContentLoaded', function(){
+  new WOW().init();
+});
+
+$('.slider').slick({
+  autoplay: true,//自動的に動き出すか。初期値はfalse。
+  autoplaySpeed: 1000,//次のスライドに切り替わる待ち時間
+  speed:1000,//スライドの動きのスピード。初期値は300。
+  infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+  slidesToShow: 1,//スライドを画面に3枚見せる
+  slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
+  arrows: false,//左右の矢印あり
+  prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+  nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+  dots: true,//下部ドットナビゲーションの表示
+      pauseOnFocus: false,//フォーカスで一時停止を無効
+      pauseOnHover: false,//マウスホバーで一時停止を無効
+      pauseOnDotsHover: false,//ドットナビゲーションをマウスホバーで一時停止を無効
+  });
+
+//スマホ用：スライダーをタッチしても止めずにスライドをさせたい場合
+$('.slider').on('touchmove', function(event, slick, currentSlide, nextSlide){
+  $('.slider').slick('slickPlay');
+});
+
+//アコーディオンをクリックした時の動作
+$('.title').on('click', function() {//タイトル要素をクリックしたら
+  var findElm = $(this).next(".box");//直後のアコーディオンを行うエリアを取得し
+  $(findElm).slideToggle();//アコーディオンの上下動作
+    
+  if($(this).hasClass('close')){//タイトル要素にクラス名closeがあれば
+    $(this).removeClass('close');//クラス名を除去し
+  }else{//それ以外は
+    $(this).addClass('close');//クラス名closeを付与
+  }
+});
+
+//ページが読み込まれた際にopenクラスをつけ、openがついていたら開く動作※不必要なら下記全て削除
+$(window).on('load', function(){
+  $('.accordion-area li:first-of-type section').addClass("open"); //accordion-areaのはじめのliにあるsectionにopenクラスを追加
+  $(".open").each(function(index, element){ //openクラスを取得
+    var Title =$(element).children('.title'); //openクラスの子要素のtitleクラスを取得
+    $(Title).addClass('close');       //タイトルにクラス名closeを付与し
+    var Box =$(element).children('.box'); //openクラスの子要素boxクラスを取得
+    $(Box).slideDown(500);          //アコーディオンを開く
+  });
+});
+
+//header > nav
+// ==================================================
+$(function(){
+  $('#nav-btn').on('click',function(){
+      $(this).toggleClass('-active');
+      $('#nav').toggleClass('-active');
+  });
+});
+
+
+
